72614b4228ecb08f3e606cdacaf66fc2cc52c22c
megablas <megablas@github.com>
2026-03-02 18:23:24 -0300

    Fix NDK version and minSdk for Firebase

--- flutter_client/android/app/build.gradle.kts
+++ flutter_client/android/app/build.gradle.kts
@@ -20,7 +20,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.blas.homesync"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -45,7 +45,7 @@ android {
         applicationId = "com.blas.homesync"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
