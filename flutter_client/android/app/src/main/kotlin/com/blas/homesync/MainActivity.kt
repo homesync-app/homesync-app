df73f1a1f17b48b186a9734f7bf84f00825966e8
megablas <megablas@github.com>
2026-05-26 09:44:50 -0300

    Prepare Android build 74 edge-to-edge

--- flutter_client/android/app/src/main/kotlin/com/blas/homesync/MainActivity.kt
+++ flutter_client/android/app/src/main/kotlin/com/blas/homesync/MainActivity.kt
@@ -2,6 +2,7 @@ package com.blas.homesync
 
 import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.emoji2.text.EmojiCompat
 import androidx.emoji2.bundled.BundledEmojiCompatConfig
@@ -11,6 +12,10 @@ class MainActivity : FlutterFragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             enableEdgeToEdge()
+            window.attributes = window.attributes.apply {
+                layoutInDisplayCutoutMode =
+                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
+            }
         }
         val config = BundledEmojiCompatConfig(this)
         EmojiCompat.init(config)
